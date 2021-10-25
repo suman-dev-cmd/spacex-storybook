fcd2d2cb3af1a91929b628aba557095549ac577f
suman-dev-cmd <sumanj@capitalnumbers.com>
2021-10-26 00:35:17 +0530

    implement of redux toolkit

--- src/state/slice/spaceSlice.ts
+++ src/state/slice/spaceSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import {getItems,getItem} from '../actions/spacexActions';
 import moment from 'moment';
 import { DataProp } from '../../components/table/tbody/Tbody';
 export interface Spacex {
@@ -87,41 +86,7 @@ const spaceSlice = createSlice({
        
         }
     },
-    extraReducers: 
-    function (builder) {
-        builder.addCase(getItems.pending, (state, action) => {
-            state.isLoading = true;
-        });
-        builder.addCase(getItems.fulfilled, (state, action: PayloadAction<Spacex[]>) => {
-            state.isLoading= false;
-            state.item = action.payload
-            state.errorMessage = '';
-        });
-        builder.addCase(getItems.rejected, (state, action) => {
-            state.isLoading = false;
-            state.item = [];
-            let msg = action.error.message;
-            if(action.error.message === 'Rejected'){
-                msg = 'Not Found Launches'
-            }
-            state.errorMessage = msg || "";
-        });
-        builder.addCase(getItem.pending, (state, action) => {
-            state.modal = false; 
-        });
-        builder.addCase(getItem.fulfilled, (state, action: PayloadAction<{flight_number:number|undefined}>) => {
-            state.modal = true;
-            state.singleItem = action.payload  
-        });
-        builder.addCase(getItem.rejected, (state, action) => {
-            state.modal = true;
-            let msg = action.error.message;
-            if(action.error.message === 'Rejected'){
-                msg = 'Not Found Flight Id'
-            }
-            state.errorMessage = msg || "";
-        });
-    }
+    
 
 });
 
